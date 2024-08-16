8436c8b571ebc191047d50b5adfd67f5ef5b5df5
Davian Affognon <affogdav000@warren.k12.in.us>
2024-08-16 11:58:12 -0400

    wa

--- src/MegaByteConverter.java
+++ src/MegaByteConverter.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class MegaByteConverter {
+public class MegaByteConverter {
+    public static void main(String[] args) {
+    }
+
+    public static void printMegaBytesAndKiloBytes(int kiloBytes){
+        if(kiloBytes < 0){
+            System.out.println("Invalid Value");
+        }
+        else{
+            int mb = kiloBytes / 1024;
+            int kb= kiloBytes % 1024;
+            System.out.println(kiloBytes + "KB = " + mb + "MB" + "and" + kb + "kb");
+        }
+        // 1mb = 1024 kb (we are plugging kb so 1 kb/ 1024 = mb % is remainder
+    }
+
 }
